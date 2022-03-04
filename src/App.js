668275862f13e2e492592efa3aa8fcbab259fb47
Johnatan Santos <jonystrit@gmail.com>
2022-03-04 17:17:10 -0300

    add scss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
 
 function App() {
   return (
     <div className="App">
-     
+
+      <Header>
+
+      </Header>
+
+      <Footer>
+
+      </Footer>
+      
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer(){
+  return(
+    <div className="footer">
+
+    </div>
+  )
+}
+
+
+export default Footer
