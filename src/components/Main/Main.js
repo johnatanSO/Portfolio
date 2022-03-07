d98c879aee003f45f162edcba9043e5431ed3ef1
Johnatan Santos <jonystrit@gmail.com>
2022-03-07 13:47:18 -0300

    skills components create

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Main.css"
+import SkillsContent from '../SkillsContent/SkillsContent'
+import Projects from '../Projects/Projects'
 
 export default function Main() {
   return (
     <main className="mainContent">
-
+      <SkillsContent></SkillsContent>
+      <Projects></Projects>
     </main>
   )
 }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <section className="skillsContent">
+        <div className="skillsText">
+          <h2>Projects</h2>
+          <p>Lorem Ipsum is simply dumetting industry. Lorem Ipsum has been the industry's standard dummy text ever since th desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div className="skillsImage">
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of lettwhen looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of lettwhen looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of lettwhen looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.</p>
+        </div>
+    </section>
+  )
+}
+
+export default Projects
