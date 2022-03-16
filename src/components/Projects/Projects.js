474ebe87321921a32f74a3b57a18987cac5c7921
Johnatan Santos <jonystrit@gmail.com>
2022-03-16 17:44:14 -0300

    images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,7 +16,7 @@ function Projects() {
           </p>
         </div>
         <div className="projectsImage">
-          {/* <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of lettwhen looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of lettwhen looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of lettwhen looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.</p> */}
+          <img alt="Imagem de projetos" src="/assets/images/13.eps" />
         </div>
       </div>
 
