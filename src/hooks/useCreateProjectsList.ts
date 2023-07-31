47beebe9eeb6b18308314f3998587b267dde2080
johnatanSO <jonystrit@gmail.com>
2023-07-31 09:49:37 -0300

    :sparkles: [FEAT] - Organizando projetos.

--- src/hooks/useCreateProjectsList.ts
+++ src/hooks/useCreateProjectsList.ts
@@ -4,62 +4,44 @@ export function useCreateProjectsList() {
       githubLink: '/sistema-de-vendas',
       title: 'comerce',
       description:
-        'Esse é um sistema para gerenciamento de dados de um comércio, com funcionalidades de cadastro de clientes, empresas, produtos e pedidos.',
-      deployLink: '/',
+        'Esse é um sistema ERP para gerenciamento de dados de um comércio, com funcionalidades de login/cadastro de conta, dashboard, cadastro de produtos, vendas, contas a pagar/receber e clientes.',
+      deployLink: 'https://sistema-de-vendas-lac.vercel.app/',
       imageLink: 'tela-de-empresas.png',
-      inRefactoring: true,
+      inRefactoring: false,
     },
     {
       githubLink: '/Sistema-Escolar',
       title: 'schoolSystem',
       description:
-        'Esse é um sistema escolar feito para controlar e gerenciar dados de alunos como: notas, faltas e advertências. Construído com o banco de dados Firebase.',
+        'Esse é um sistema escolar feito para controlar e gerenciar dados de alunos de uma escola como: notas, faltas e advertências. O sistema é dividido em duas áreas, uma para professores e uma para alunos com permissões diferentes para cada um. Construído com NextJS e SASS modules no front-end e Node e Express no back-end com o banco de dados MongoDB. O sistema possui também uma versão mobile construída com React Native.',
       deployLink:
         'https://sistema-escolar-delta.vercel.app/pages/teacher/index.html',
       imageLink: 'schoolPrint.png',
       toRefactor: true,
     },
     {
+      githubLink: '/To-Do-List-React',
+      title: 'todo',
+      description:
+        'Esse é um projeto de uma to-do list, feita com NextJS e SASS modules, para que você consiga organizar suas tarefas da maneira mais prática possível e otimizar o seu tempo.',
+      deployLink: 'https://to-do-list-react-bice.vercel.app/',
+      imageLink: 'todoPrint.png',
+      toRefactor: false,
+    },
+    {
       githubLink: '/api-films',
       title: 'apiFilms',
       description:
-        'Esse é um projeto de uma biblioteca de filmes e séries construído com ReactJS e usando a API RESTful OMdb.',
+        'Esse é um projeto de uma biblioteca para busca de filmes e séries construído usando NextJS e a API OMdb.',
       deployLink: 'https://api-films.vercel.app/',
       imageLink: 'moviesPrint.png',
       inRefactoring: true,
     },
     {
-      githubLink: '/dtmoney',
-      title: 'money',
-      description:
-        'Esse é uma aplicação de um sistema para controle financeiro.',
-      deployLink: 'dtmoney/',
-      imageLink: 'dtmoney.png',
-      toRefactor: true,
-    },
-    {
-      githubLink: '/To-Do-List-React',
-      title: 'todo',
-      description:
-        'Esse é um projeto de uma to-do list para que você consiga organizar suas tarefas da maneira mais prática possível.',
-      deployLink: 'https://to-do-list-react-bice.vercel.app/',
-      imageLink: 'todoPrint.png',
-      toRefactor: true,
-    },
-    {
-      githubLink: '/pokedexAPI',
-      title: 'pokedexAPI',
-      description:
-        'Esse é um projeto de uma pokédex construído com ReactJS e usando a API RESTful pokéAPI, que lista todos os Pokemons existentes e seus atributos.',
-      deployLink: 'https://pokedex-api-virid.vercel.app/',
-      imageLink: 'pokePrint.png',
-      toRefactor: true,
-    },
-    {
       githubLink: '/JOGO-DA-MEMORIA',
       title: 'memoryGame',
       description:
-        'Um jogo da memória simples para que você possa se distraír um pouco de forma rápida. Construído com HTML, CSS e Javascript puro.',
+        'Um jogo da memória simples para que você possa se distrair um pouco de forma rápida. Construído com HTML, CSS e Javascript puro.',
       deployLink: 'https://jogo-da-memoria-snowy.vercel.app/',
       imageLink: 'memoryPrint.png',
       toRefactor: true,
@@ -71,15 +53,6 @@ export function useCreateProjectsList() {
         'Um clássico jogo da velha para que você possa jogar com algum amigo e se divertir. Construído com HTML, CSS e Javascript puro.',
       deployLink: 'https://johnatanso.github.io/JOGO-DA-VELHA/',
       imageLink: 'jogoDaVelhaPrint.png',
-      toRefactor: true,
-    },
-    {
-      githubLink: '/music-player',
-      title: 'musicPlayer',
-      description:
-        'Um player de música para você ouvir as suas músicas favoritas enquanto faz qualquer coisa durante o dia. Construído com HTML, CSS e Javascript puro.',
-      deployLink: 'https://johnatanso.github.io/music-player/',
-      imageLink: 'playerPrint.png',
       toRefactor: true,
     },
   ]
