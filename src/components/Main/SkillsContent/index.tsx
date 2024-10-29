a3ad4630757e8dd02adb6e3f87e7eb992575b32a
JohnatanSO <devsantosjohn@gmail.com>
2024-10-29 19:56:31 -0300

    ci

--- src/components/Main/SkillsContent/index.tsx
+++ src/components/Main/SkillsContent/index.tsx
@@ -45,9 +45,10 @@ export function SkillsContent() {
         <p>
           Além disso, tenho experiência em ferramentas e tecnologias importantes
           como <b>Docker, GIT, GITHUB, Firebase</b>, práticas de{' '}
-          <b>CI/CD, Clean Code, SOLID,</b> testes automatizados com <b>JEST,</b>{' '}
-          que me permitem desenvolver soluções completas, seguras e integradas,
-          desde a fase de desenvolvimento até a implementação.
+          <b>CI/CD e configuração de pipelines, Clean Code, SOLID,</b> testes
+          automatizados com <b>JEST,</b> que me permitem desenvolver soluções
+          completas, seguras e integradas, desde a fase de desenvolvimento até a
+          implementação.
         </p>
       </div>
     </section>
